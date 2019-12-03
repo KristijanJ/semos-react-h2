346b7167557ecc62182d2cdcccc0057ba9a10903
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2019-12-03 11:27:28 +0100

    Updates

---
+++ precache-manifest.75177d413204f2c14f4f6b542a8510bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda887f62681cf537a26d9bab229ef7c",
+    "url": "/semos-react-h2/index.html"
+  },
+  {
+    "revision": "503ad1741071e73a14f9",
+    "url": "/semos-react-h2/static/js/2.324a4ffd.chunk.js"
+  },
+  {
+    "revision": "c1273a557a7f55d41307",
+    "url": "/semos-react-h2/static/js/main.bdb4a7bb.chunk.js"
+  },
+  {
+    "revision": "b14010ed06a0afc67f04",
+    "url": "/semos-react-h2/static/js/runtime-main.0259e99d.js"
+  }
+]);
